e078c22e3b55275a40c069a4553d1456b744283e
oscar ortiz <oscar.ortiz@behit.cat>
2024-05-30 16:23:38 -0600

    se agrega comparacion de password listo para implementar jwt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,41 @@
 const response = require('express');
+const bcryptjs = require('bcryptjs');
+const User = require('../models/user.model')
+
+const login = async (req, res = response) => {
+
+    const {email, password} = req.body;
 
 
-const login = async (req, res) => {
+    try {
+        const userDB = await User.findOne({ email });
 
-    res.json({
-        ok: true,
-        msg:'Hola mundo'
-    })
+        if (!userDB) {
+            return res.status(404).json({
+                ok:false,
+                msg:'email no encontrado'
+            })
+        }
+
+        const validPassword = bcryptjs.compareSync( password, userDB.password );
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok:false,
+                msg:'contraseña no encontrada'
+            })
+        }
+
+        // GENERA JWT
+
+        res.json({
+            ok: true,
+            msg:'Hola mundo'
+        })
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 module.exports = {
